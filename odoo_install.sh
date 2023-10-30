e953ac8e0d386cc8130f97f46eb1465e46a782fb
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-10-30 11:31:25 +0100

    add newest pyopenssl and cryptography dependencies

--- odoo_install.sh
+++ odoo_install.sh
@@ -26,24 +26,24 @@ OE_PORT="8069"
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 16.0
 OE_VERSION="16.0"
 # Set this to True if you want to install the Odoo enterprise version!
-IS_ENTERPRISE="False"
+IS_ENTERPRISE="True"
 # Installs postgreSQL V14 instead of defaults (e.g V12 for Ubuntu 20/22) - this improves performance
 INSTALL_POSTGRESQL_FOURTEEN="True"
 # Set this to True if you want to install Nginx!
-INSTALL_NGINX="False"
+INSTALL_NGINX="True"
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
 OE_SUPERADMIN="admin"
 # Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="_"
+WEBSITE_NAME="odoo.beopen.cloud"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
 ENABLE_SSL="True"
 # Provide Email to register ssl certificate
-ADMIN_EMAIL="odoo@example.com"
+ADMIN_EMAIL="carlos.rodriguez@avvale.com"
 ##
 ###  WKHTMLTOPDF download links
 ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
@@ -106,6 +106,22 @@ sudo -H pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requireme
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
 sudo npm install -g rtlcss
+
+
+#--------------------------------------------------
+# Install Dependencies newest dependencies:
+#--------------------------------------------------
+# pyopenssl==20.0.1 by pyopenssl==22.0.0
+# cryptography==3.4.8 by cryptography==37.0.0
+#
+# otherwise installation fails
+#--------------------------------------------------
+echo -e "\n---- Fixed versions of pyopenssl & cryptography ----"
+sudo pip3 uninstall pyopenssl
+sudo pip3 install pyopenssl==22.0.0
+
+sudo pip3 uninstall cryptography
+sudo pip3 install cryptography==37.0.0
 
 #--------------------------------------------------
 # Install Wkhtmltopdf if needed
